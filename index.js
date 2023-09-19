c73a4be474b111e4b6430190850c7303487fc2b0
njimmy10 <90354662+njimmy10@users.noreply.github.com>
2023-09-19 14:26:51 +0300

    Express

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 var db_connection = require('./mysql/config.js');
 require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}
+)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+}
+)
 
 
 
